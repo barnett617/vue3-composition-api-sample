9b3064ce3ff4ce136b7b7111702fed7d2a379c96
wangxin <wangxina@didichuxing.com>
2020-10-10 15:13:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <computedProperties />
+</template>
+
+<script>
+import computedProperties from './components/vue3-computed-properties.vue'
+
+export default {
+  name: 'App',
+  components: {
+    computedProperties
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/vue3-computed-properties.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>Spaces Left: {{ spacesLeft }} of {{ capacity }}</p>
+    <button @click="increaseCapacity()">Increase Capacity</button>
+    <h2>Attending</h2>
+    <ul>
+      <li v-for="(name, index) in attending" :key="index">
+        {{ name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { ref, computed } from 'vue'
+export default {
+  setup() {
+    const capacity = ref(3)
+    const attending = ref(["Tim", "Bob", "Joe"])
+    function increaseCapacity() {
+      capacity.value++
+    }
+    const spacesLeft = computed(() => {
+      return capacity.value - attending.value.length
+    })
+
+    return { capacity, increaseCapacity, attending, spacesLeft }
+  }
+}
+</script>
